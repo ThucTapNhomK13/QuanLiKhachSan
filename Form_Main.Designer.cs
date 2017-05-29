8844fdff751e4edbfc75fdac04cc472697145e75
vtait257 <4l3xvu.tu4n4nh@gmail.com>
2017-05-29 22:21:03 +0700

    Help
    Done

---
+++ Form_Main.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace GUI
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labName = new MetroFramework.Controls.MetroLabel();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroLink1 = new MetroFramework.Controls.MetroLink();
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroTile3 = new MetroFramework.Controls.MetroTile();
+            this.metroTile4 = new MetroFramework.Controls.MetroTile();
+            this.SuspendLayout();
+            // 
+            // labName
+            // 
+            this.labName.AutoSize = true;
+            this.labName.Location = new System.Drawing.Point(540, 101);
+            this.labName.Name = "labName";
+            this.labName.Size = new System.Drawing.Size(0, 0);
+            this.labName.TabIndex = 0;
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(128, 140);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(199, 79);
+            this.metroTile1.TabIndex = 1;
+            this.metroTile1.Text = "Danh sách khách hàng";
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.Click += new System.EventHandler(this.metroTile1_Click);
+            // 
+            // metroLink1
+            // 
+            this.metroLink1.FontSize = MetroFramework.MetroLinkSize.Tall;
+            this.metroLink1.FontWeight = MetroFramework.MetroLinkWeight.Regular;
+            this.metroLink1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.metroLink1.ImageSize = 40;
+            this.metroLink1.Location = new System.Drawing.Point(27, 73);
+            this.metroLink1.Name = "metroLink1";
+            this.metroLink1.Size = new System.Drawing.Size(128, 49);
+            this.metroLink1.TabIndex = 2;
+            this.metroLink1.Text = "Đăng xuất";
+            this.metroLink1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.metroLink1.UseSelectable = true;
+            this.metroLink1.Click += new System.EventHandler(this.metroLink1_Click);
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.ActiveControl = null;
+            this.metroTile2.Location = new System.Drawing.Point(128, 225);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(199, 79);
+            this.metroTile2.TabIndex = 3;
+            this.metroTile2.Text = "Danh sách hóa đơn";
+            this.metroTile2.UseSelectable = true;
+            this.metroTile2.Click += new System.EventHandler(this.metroTile2_Click);
+            // 
+            // metroTile3
+            // 
+            this.metroTile3.ActiveControl = null;
+            this.metroTile3.Location = new System.Drawing.Point(333, 140);
+            this.metroTile3.Name = "metroTile3";
+            this.metroTile3.Size = new System.Drawing.Size(191, 164);
+            this.metroTile3.TabIndex = 1;
+            this.metroTile3.Text = "Danh sách phòng";
+            this.metroTile3.UseSelectable = true;
+            this.metroTile3.Click += new System.EventHandler(this.metroTile3_Click);
+            // 
+            // metroTile4
+            // 
+            this.metroTile4.ActiveControl = null;
+            this.metroTile4.Location = new System.Drawing.Point(128, 310);
+            this.metroTile4.Name = "metroTile4";
+            this.metroTile4.Size = new System.Drawing.Size(199, 51);
+            this.metroTile4.TabIndex = 4;
+            this.metroTile4.Text = "Hướng dẫn sử dụng";
+            this.metroTile4.UseSelectable = true;
+            this.metroTile4.Click += new System.EventHandler(this.metroTile4_Click);
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 387);
+            this.Controls.Add(this.metroTile4);
+            this.Controls.Add(this.metroTile2);
+            this.Controls.Add(this.metroLink1);
+            this.Controls.Add(this.metroTile3);
+            this.Controls.Add(this.metroTile1);
+            this.Controls.Add(this.labName);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form_Main";
+            this.Padding = new System.Windows.Forms.Padding(23, 69, 23, 23);
+            this.Text = "Trang chủ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Main_FormClosing);
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel labName;
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroLink metroLink1;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroTile metroTile3;
+        private MetroFramework.Controls.MetroTile metroTile4;
+    }
+}
